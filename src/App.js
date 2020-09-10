0a7664ab368f7d5c34b76068b031f6b4ab05ef5f
Mohammed Shafeer <abuabdullah.sed@gmail.com>
2020-09-10 19:51:30 +0530

    With the name of ALLAH

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>There is no one worthy of worship except ALLAH</p>
+        <p>All Praise is to ALLAH</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
